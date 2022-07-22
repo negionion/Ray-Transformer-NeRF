d730613785a3a6e7bdcc26f14964f240fc6bbb7a
NEGIm <31944608+negionion@users.noreply.github.com>
2022-07-22 14:55:06 +0800

    Add GeLU

--- src/model/resnetfc.py
+++ src/model/resnetfc.py
@@ -40,8 +40,11 @@ class ResnetBlockFC(nn.Module):
 
         if beta > 0:
             self.activation = nn.Softplus(beta=beta)
-        else:
+        elif beta == 0:
             self.activation = nn.ReLU()
+        elif beta < 0:
+            self.activation = nn.GeLU()
+            
 
         if size_in == size_out:
             self.shortcut = None
@@ -126,8 +129,10 @@ class ResnetFC(nn.Module):
 
         if beta > 0:
             self.activation = nn.Softplus(beta=beta)
-        else:
+        elif beta == 0:
             self.activation = nn.ReLU()
+        elif beta < 0:
+            self.activation = nn.GeLU()
 
     def forward(self, zx, combine_inner_dims=(1,), combine_index=None, dim_size=None):
         """
